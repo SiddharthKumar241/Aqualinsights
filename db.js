b00d6ee2de3724d56b1381c51f394d40929ccff0
SiddharthKumar241 <siddharth.goutam2023@vitstudent.ac.in>
2025-04-06 15:51:30 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://kumarsiddharth166:DarkSister@clusterone.bqlr6.mongodb.net/waterDB", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://.bqlr6.mongodb.net/waterDB", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
